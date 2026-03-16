2efe5666171971d8ac677a3469d17123fc584b3c
Naomi Torres <ivanys.ntm@gmail.com>
2026-03-16 00:44:28 -0700

    all new changes

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,3 @@
-console.log("JS connected");
-
 'use strict';
 
 // Store Hours
@@ -9,125 +7,67 @@ const hours = [
     '6pm','7pm'
 ];
 
+const container = document.getElementById('sales-data');
+
+function randomCustomers(minCustomers,maxCustomers){
+    return Math.floor(Math.random()*(maxCustomers-minCustomers+1)+minCustomers);
+}
+
+
 // Seattle Store Object
 const seattle = {
     name: 'Seattle',
     minCustomers: 23,
     maxCustomers: 65,
     avgCookies: 6.3,
-    cookiesPerHour: [],
-    totalCookies: 0
-};
+    Sales: [],
+    totalCookies: 0,
 
-seattle.randomCustomers = function() {
-    return Math.floor(Math.random() *
-    (this.maxCustomers - this.minCustomers + 1)
-    + this.minCustomers);
-};
+    calculateCookies:function(){
+        for(let i=0;i<hours.length,i++){
+            let customers=randomCustomers(this.minCustomers,this.maxCustomers);
+            let cookies=Math.floor(customers*this.avgCookies);
+            this.Sales.push(cookies);
+            this.totalCookies+=cookies;
+        }
+    },
 
-seattle.calculateCookies = function() {
+    render.function(){
 
-    for(let i = 0; i < hours.length; i++){
+        let article=document.createElement('article');
+        let h2=document.createElement('h2');
 
-        let customers = this.randomCustomers();
-let cookies = Math.floor(customers * this.avgCookies);
+        h2.textContent=this.name;
 
-        this.cookiesPerHour.push(cookies);
-his.totalCookies += cookies;
+        article.appendChild(h2);
+
+        let ul=document.createElement('ul');
+
+        for(let i=0;i<hours.length;i++){
+
+            let li=document.createElement('li');
+            li.textContent=`${hours[i]}: ${this.Sales[i]} cookies`;
+            
+            ul.appendChild(li);
+
+        }
+
+        let.totalCookies=document.createElement('li');
+        totalCookies.textContent=`Total: ${this.totalCookies} cookies`;
+
+        ul.appendChild(totalCookies);
+
+        article.appendChild(ul);
+
+        container.appendChild(article);
     }
-},
-
-
-seattle.render = function() {
-
-    let container = document.getElementById('sales-data');
-
-    let article = document.createElement('article');
-
-    let h2 = document.createElement('h2');
-    h2.textContent = this.name;
-    article.appendChild(h2);
-
-    let ul = document.createElement('ul');
-
-    for(let i = 0; i < hours.length; i++){
-
-        let li = document.createElement('li');
-
-        li.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies`;
-
-        ul.appendChild(li);
-    }
-
-    let total = document.createElement('li');
-total.textContent = `Total: ${this.total} cookies`;
-
-    ul.appendChild(total);
-
-    article.appendChild(ul);
-
-    container.appendChild(article);
-
 };
 
-//Tokyo Store Object
-const tokyo = {
-    name: 'Tokyo',
-    minCustomers: 3,
-    maxCustomers: 24,
-    avgCookies: 1.2,
-    cookiesPerHour: [],
-    total: 0,
+const tokyo={name:'Tokyo',minCustomers:3,maxCustomers:24,avgCookies:1.2,Sales:[],totalCookies:0,calculateCookies:seattle.calculateCookies,render:seattle.render};
+const dubai={name:'Dubai',minCustomers:11,maxCustomers38,avgCookies:3.7,Sales:[],totalCookies:0,calculateCookies:seattle.calculateCookies,render:seattle.render};
+const paris={name:'Paris',minCustomers:20,maxCustomers38,avgCookies:2.3,Sales:[],totalCookies:0,calculateCookies:seattle.calculateCookies,render:seattle.render};
+const lima={name:'Lima',minCustomers:2,maxCustomers:16,avgCookies:4.6,sales:[],totalCookies:0,calculateCookies:seattle.calculateCookies,render:seattle.render};
 
-    randomCustomers: seattle.randomCustomers,
-    calculateCookies: seattle.calculateCookies,
-    render: seattle.render
-
-};
-
-//Dubai Store Object
-const dubai = {
-    name: 'Dubai',
-    minCustomers: 11,
-    maxCustomers: 38,
-    avgCookies: 3.7,
-    cookiesPerHour: [],
-    total: 0,
-
-    randomCustomers: seattle.randomCustomers,
-    calculateCookies: seattle.calculateCookies,
-    render: seattle.render
-
-};
-
-//Paris Store Object 
-const paris = {
-    name: 'Paris',
-    minCustomers: 20,
-    maxCustomers: 38,
-    avgCookies: 2.3,
-    cookiesPerHour: [],
-    total: 0,
-
-    randomCustomers: seattle.randomCustomers,
-    calculateCookies: seattle.calculateCookies,
-    render: seattle.render
-
-};
-
-//Lima Store Object 
-const lima = {
-    name: 'Lima',
-    minCustomers: 2,
-    maxCustomers: 16,
-    avgCookies: 4.6,
-    cookiesPerHour: [],
-    total: 0,
-
-      randomCustomers: seattle.randomCustomers,
-    calculateCookies: seattle.calculateCookies,
-    render: seattle.render
-};
 
 seattle.calculateCookies();
 seattle.render();
